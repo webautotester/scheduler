5f888237793b2f2baf4235c178487099015cff62
Xavier Blanc <xavier.blanc@labri.fr>
2017-09-14 10:19:40 +0200

    simplification of the routes. add unschedule service

--- routes/play.js
+++ routes/play.js
@@ -8,82 +8,123 @@ const QUEUE_NAME = 'wat_queue';
 module.exports.init = function(serverNames, webServer) {
 	const dbUrl = `mongodb://${serverNames.mongoServerName}:27017/wat_storage`;
 	const rmqUrl = `amqp://${serverNames.rabbitServerName}`;
-	webServer.post('/play', (req, res) => {
-		const playOptions = req.body;
-		if (playOptions.now) {
-			playNow(req,res, playOptions);
-		} else {
-			addCron(req, res, playOptions)
-		}
-	});
-
-	function playNow(req, res, playOptions) {
-		winston.info(`Play Now Request on ${dbUrl}`);
-		MongoClient.connect(dbUrl).then(db => {
-			db.collection('scenario', (err, scenarioCollection) => {
-				if (err) {
-					winston.error(`Play Now Request Error : ${err}`);
-					db.close();
-					res.send(err).status(404).end();
-				} else {
-					winston.info(`Launch promise ${playOptions.sid}`);
-					var firstPromise = scenarioCollection.find({_id:new ObjectID(playOptions.sid)}).toArray();
-					var secondPromise = amqp.connect(rmqUrl).then( conn => {return conn.createConfirmChannel()}).catch( e=> {return Promise.reject(e)});
-					Promise.all([firstPromise,secondPromise]).then(promizesResults => {
-						winston.info('Play Now Request ');
-						var scenarioToPlay = promizesResults[0][0];
-						var channel = promizesResults[1];
-						var msg = JSON.stringify({ sid:scenarioToPlay._id , actions: scenarioToPlay.actions })
-						winston.info(msg);
-						channel.assertQueue(QUEUE_NAME, { durable: true })
-						.then(ok => {
-							if (ok) {
-								return channel.sendToQueue(QUEUE_NAME, Buffer.from(msg), {persistent: true});
-							} else {
-								return Promise.reject(ok);
-							}
-						}).then(() => {
-							channel.close();
-							db.close();
-							res.send(`play request sent for scenario ${playOptions.sid}`).status(200).end();
-						});
-					})
-					.catch(err => {
-						db.close();
-						res.send(err).status(500).end();
-					})
-				}
-			});
-			
-		}).catch(err => {
-			winston.info(err);
-			res.send(err).status(500).end;
+	webServer
+		.get('/playNow/:sid', (req, res) => {
+			playNow(req,res, req.params.sid);
+		})
+		.get('/schedule/:sid', (req, res) => {
+			//each minute for test purpose
+			//will be each day in beta production
+			const options = {
+				cron : '* * * * *',
+				sid : req.params.sid
+			};
+			addCron(req,res, options);
+		})
+		.get('/unschedule/:sid', (req, res) => {//daily
+			removeCron(req,res, req.params.sid);
 		});
+
+	function playNow(req, res, sid) {
+		winston.info(`Play Now Request on ${dbUrl}`);
+		MongoClient.connect(dbUrl)
+			.then(db => {
+				db.collection('scenario', (err, scenarioCollection) => {
+					if (err) {
+						winston.error(`Play Now Request Error : ${err}`);
+						db.close();
+						res.status(404).send(err).end();
+					} else {
+						winston.info(`Launch promise ${sid}`);
+						var firstPromise = scenarioCollection.find({_id:new ObjectID(sid)}).toArray();
+						var secondPromise = amqp.connect(rmqUrl)
+							.then( conn => {
+								return conn.createConfirmChannel();
+							})
+							.catch( e=> {
+								return Promise.reject(e);
+							});
+						Promise.all([firstPromise,secondPromise])
+							.then(promizesResults => {
+								winston.info('Play Now Request ');
+								var scenarioToPlay = promizesResults[0][0];
+								var channel = promizesResults[1];
+								var msg = JSON.stringify({ sid:scenarioToPlay._id , actions: scenarioToPlay.actions });
+								winston.info(msg);
+								channel.assertQueue(QUEUE_NAME, { durable: true })
+									.then(ok => {
+										if (ok) {
+											return channel.sendToQueue(QUEUE_NAME, Buffer.from(msg), {persistent: true});
+										} else {
+											return Promise.reject(ok);
+										}
+									})
+									.then(() => {
+										channel.close();
+										db.close();
+										res.status(200).send(`play request sent for scenario ${sid}`).end();
+									})
+									.catch ((err) =>{
+										channel.close();
+										db.close();
+										winston.error(err);
+										res.status(500).send(`play request cannot be sent : ${err}`);
+									});
+							})
+							.catch(err => {
+								db.close();
+								res.status(500).send(err).end();
+							});
+					}
+				});
+			
+			}).catch(err => {
+				winston.info(err);
+				res.send(err).status(500).end;
+			});
 	}
 
 	function addCron(req, res, playOptions) {
 		const cron = playOptions.cron;
+		const comment = playOptions.sid;
 		winston.info(`Add new cron for scenario (${playOptions.sid}) with cron ${cron}`);
 		if (cron) {
-			Crontab.load(function(err, ct) {
+			Crontab.load((err, ct) => {
 				// create with string expression
-				const NODE_BIN = '/usr/local/n/versions/node/8.4.0/bin/node';
+				const NODE_BIN = '/usr/local/bin/node';
 				const NODE_SCRIPT = '/tmp/scheduler/sendPlayMessage.js';
 				const NODE_OPTIONS = `--mongo=mongo --rabbit=rabbit --sid=${playOptions.sid}`;
-				const LOG = `>> /var/log/watcron.log 2>&1`;
-				var job = ct.create(`${NODE_BIN} ${NODE_SCRIPT} ${NODE_OPTIONS} ${LOG}`, cron);
+				const LOG = '>> /var/log/watcron.log 2>&1';
+				ct.create(`${NODE_BIN} ${NODE_SCRIPT} ${NODE_OPTIONS} ${LOG}`, cron, comment);
 				
-				ct.save(function(err, newCrontab) {
+				ct.save((err) => {
 					if (err) {
-						res.send(err).status(500).end();
+						res.status(500).send(err).end();
 					}
 					else {
-						res.send(`cron request sent for scenario ${playOptions.sid}`).status(200).end();
+						res.status(200).send(`cron request sent for scenario ${playOptions.sid}`).end();
 					}
 				}); 
 			});
 		} else {
-			res.send(`cron ${cron} is not valid`).status(500).end();
+			res.status(500).send(`cron ${cron} is not valid`).end();
 		}
 	}
+
+	function removeCron(req, res, sid) {
+		winston.info(`Remove cron for scenario (${sid})`);
+		
+		Crontab.load((err, ct) => {
+			ct.remove({comment: sid});
+			ct.save((err) => {
+				if (err) {
+					res.status(500).send(err).end();
+				}
+				else {
+					res.status(200).send(`${sid} removed from the list`).end();
+				}
+			}); 
+		});
+	}
+	
 };
--- sendPlayMessage.js
+++ sendPlayMessage.js
@@ -2,13 +2,13 @@ const winston = require('winston');
 const amqp = require('amqplib');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
-const QUEUE_NAME = 'wat_queue'
+const QUEUE_NAME = 'wat_queue';
 
 const argv = require('yargs').argv;
 const serverNames = {
-    mongoServerName : argv.mongo,
-    rabbitServerName : argv.rabbit    
-}
+	mongoServerName : argv.mongo,
+	rabbitServerName : argv.rabbit    
+};
 const sid = argv.sid;
 
 const dbUrl = `mongodb://${serverNames.mongoServerName}:27017/wat_storage`;
@@ -17,46 +17,53 @@ const rmqUrl = `amqp://${serverNames.rabbitServerName}`;
 playNow(sid);
 
 function playNow(sid) {
-	MongoClient.connect(dbUrl).then(db => {
-		db.collection('scenario', (err, scenarioCollection) => {
-			if (err) {
-				db.close();
-				winston.error(err);
-			} else {
-				var firstPromise = scenarioCollection.find({_id:new ObjectID(sid)}).toArray();
-				var secondPromise = amqp.connect(rmqUrl).then( conn => {return conn.createConfirmChannel()}).catch( e=> {return Promise.reject(e)});
-				Promise.all([firstPromise,secondPromise])
-				.then(promizesResults => {
-					winston.info('Play Crone Request ');
-					var scenarioToPlay = promizesResults[0][0];
-					var channel = promizesResults[1];
-					var msg = JSON.stringify({ sid:scenarioToPlay._id , actions: scenarioToPlay.actions })
-					winston.info(msg);
-					channel.assertQueue(QUEUE_NAME, { durable: true })
-					.then(ok => {
-						if (ok) {
-							channel.sendToQueue(QUEUE_NAME, Buffer.from(msg), {persistent: true}, (err, succ) => {
-								winston.info(`Cron request sent for scenario ${sid}`);
-								process.exit();
-								return succ;
-							});
-						} else {
-							return Promise.reject(ok);
-						}
-					}).then(() => {
-						db.close();						
-					});
-				})
-				.catch(err => {
+	MongoClient.connect(dbUrl)
+		.then(db => {
+			db.collection('scenario', (err, scenarioCollection) => {
+				if (err) {
+					db.close();
 					winston.error(err);
-					process.exit();
-				})
-			}
+				} else {
+					var firstPromise = scenarioCollection.find({_id:new ObjectID(sid)}).toArray();
+					var secondPromise = amqp.connect(rmqUrl)
+						.then( conn => {
+							return conn.createConfirmChannel();
+						})
+						.catch( e=> {
+							return Promise.reject(e);
+						});
+					Promise.all([firstPromise,secondPromise])
+						.then(promizesResults => {
+							winston.info('Play Crone Request ');
+							var scenarioToPlay = promizesResults[0][0];
+							var channel = promizesResults[1];
+							var msg = JSON.stringify({ sid:scenarioToPlay._id , actions: scenarioToPlay.actions });
+							winston.info(msg);
+							channel.assertQueue(QUEUE_NAME, { durable: true })
+								.then(ok => {
+									if (ok) {
+										channel.sendToQueue(QUEUE_NAME, Buffer.from(msg), {persistent: true}, (err, succ) => {
+											winston.info(`Cron request sent for scenario ${sid}`);
+											process.exit();
+											return succ;
+										});
+									} else {
+										return Promise.reject(ok);
+									}
+								}).then(() => {
+									db.close();						
+								});
+						})
+						.catch(err => {
+							winston.error(err);
+							process.exit();
+						});
+				}
+			});
+		}).catch(err => {
+			winston.error(err);
+			process.exit();
 		});
-	}).catch(err => {
-		winston.error(err);
-		process.exit();
-	});
 }
 
 	
